10dee4569aebf6ad84f31ddf5c2cc20e977bca4c
Ricardo Malafaia <rbm4@cin.ufpe.br>
2018-03-30 16:07:28 +0000

    get method hotfix

--- lib/shapeshiftio.rb
+++ lib/shapeshiftio.rb
@@ -19,13 +19,13 @@ private
 public
   def self.get(path_name, value = nil, p2 = nil)
     route = ""
-    route.append(path_name)
+    route.append(path_name.to_s + "/")
     if !(value.nil?)
-      route.append(value)
+      route.append(value.to_s + "/")
     end
     
     if !(p2.nil?)
-      route.append(p2)
+      route.append(p2.to_s + "/")
     end
     uri = URI("https://shapeshift.io/#{route}")
     response = Net::HTTP.get(uri)
