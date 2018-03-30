23b720bb3f7b299437498eea240dd2cab80c3bee
Ricardo Malafaia <rbm4@cin.ufpe.br>
2018-03-30 05:15:50 +0000

    api_call return object

--- lib/shapeshiftio.rb
+++ lib/shapeshiftio.rb
@@ -12,7 +12,7 @@ private
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = (url.scheme == "https")
     response = http.request(req)
-    result = response.body.to_json
+    result = response.body.to_hash
   end
 
 
